e77a72904b4e4520fa6fcd68d4c9ac6b18e8b676
Your Name~Rian <you@example.comrian_beckert@hotmail.com>
2023-12-11 10:29:02 -0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+
+import "./MovieGrid.css";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    //Chamando a API com o fetch
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`; // Usado para chamar os melhores filmes da API
+
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="title">Melhores filmes</h2>
+      <div className="movies-container">
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
